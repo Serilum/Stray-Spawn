e9b6514bb63e9dccc5f001f4116759bb2d007332
Rick South <rick@natamus.com>
2024-07-28 07:13:03 +0200

    (v3.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/strayspawn/util/Reference.java
+++ Common/src/main/java/com/natamus/strayspawn/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.strayspawn.util;
 public class Reference {
 	public static final String MOD_ID = "strayspawn";
 	public static final String NAME = "Stray Spawn";
-	public static final String VERSION = "3.6";
+	public static final String VERSION = "3.7";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
--- Forge/src/main/java/com/natamus/strayspawn/ModForge.java
+++ Forge/src/main/java/com/natamus/strayspawn/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.strayspawn;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.strayspawn.forge.config.IntegrateForgeConfig;
 import com.natamus.strayspawn.util.Reference;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,6 +14,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
