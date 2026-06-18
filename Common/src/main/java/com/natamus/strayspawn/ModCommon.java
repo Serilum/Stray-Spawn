3511f9a5b894290ab0e1f52346eb0b5eb7079a2e
Rick South <rick@natamus.com>
2026-06-18 21:18:00 +0200

    Updated Stray Spawn to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/strayspawn/ModCommon.java
+++ Common/src/main/java/com/natamus/strayspawn/ModCommon.java
@@ -2,7 +2,7 @@ package com.natamus.strayspawn;
 
 import com.natamus.collective.objects.SAMObject;
 import com.natamus.strayspawn.config.ConfigHandler;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EntityTypes;
 import net.minecraft.world.item.Items;
 
 public class ModCommon {
@@ -13,6 +13,6 @@ public class ModCommon {
 	}
 
 	private static void load() {
-		new SAMObject(EntityType.SKELETON, EntityType.STRAY, Items.BOW, ConfigHandler.chanceSkeletonIsStray, false, false, false);
+		new SAMObject(EntityTypes.SKELETON, EntityTypes.STRAY, Items.BOW, ConfigHandler.chanceSkeletonIsStray, false, false, false);
 	}
 }
